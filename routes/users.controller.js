21cfa81ef52458f4523f4d6292f26027910f3d4a
Fatima-Athar <fatimaathar11@gmail.com>
2021-10-31 14:01:18 +0500

    Initial commit

---
+++ routes/users.controller.js
@@ -0,0 +1,15 @@
+﻿﻿/*const userController = require('../controllers/users.controller');
+const express = require('express');
+const router = express.Router();
+
+// routes
+router.post('/login', userController.login);
+router.post('/register', userController.register);
+router.get('/', userController.getAll);
+router.get('/current', userController.getCurrent);
+router.get('/:id', userController.getById);
+router.put('/:id', userController.update);
+router.delete('/:id', userController._delete);
+
+module.exports = router;
+*/
